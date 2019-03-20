e72d6dad34cfdac6687635e501ad2f409af68dfe
EC2 Default User <ec2-user@ip-172-31-15-142.us-east-2.compute.internal>
2019-03-20 04:55:29 +0000

    Add GW

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "fk, honvi!"
+    render html: "goodbye, world!"
   end
 end
